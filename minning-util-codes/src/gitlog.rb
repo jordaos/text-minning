c44a73f548f977ee02ab5ed3c56c7e6a86234025
Jordão Macedo <jordao05@hotmail.com>
2018-03-24 12:42:07 -0300

    gitlog.rb gets only commits that aren't in prev.

--- minning-util-codes/src/gitlog.rb
+++ minning-util-codes/src/gitlog.rb
@@ -3,8 +3,14 @@
 require 'git'
 require 'sqlite3'
 
-help_and_quit if ARGV.size < 2
+def help_and_quit
+  puts 'params: DBPATH, PART, REPOPATH'
+  exit 0
+end
+
+help_and_quit if ARGV.size < 3
 DBPATH = ARGV.shift
+PART = Integer(ARGV.shift)
 ALLREPOPATHS = ARGV
 
 def main
@@ -12,15 +18,16 @@ def main
     project = File.basename(repo_path).gsub('__', '/')
     puts "Extracting log from #{project}"
     db = init_database(DBPATH)
+    arr_anterior = []
+    if (PART > 1)
+      arr_anterior = previous_db()
+    end
     commit_log(repo_path) do |commit|
-      insert_commit(db, commit, project)
+      if !arr_anterior.include? commit
+        insert_commit(db, commit, project)
+      end
     end
   end
-end
-
-def help_and_quit
-  puts 'params: DBPATH, REPOPATH'
-  exit 0
 end
 
 def insert_commit(db, commit, project)
@@ -53,6 +60,23 @@ def init_database(db_path)
   );
   SQL
   db
+end
+
+def previous_db()
+  n_prev = PART - 1
+  sep = DBPATH.split('/')
+  sep.pop
+  sep.pop
+  dbs_path = sep.join('/')
+  db_path = "#{dbs_path}/#{n_prev}_part/#{n_prev}_part.sqlite"
+  db = SQLite3::Database.open db_path
+  stm = db.prepare "SELECT sha FROM commits" 
+  rs = stm.execute 
+  commits = []
+  rs.each do |commit|
+    commits.push(commit.first)
+  end
+  commits
 end
 
 def commit_log(repo_path)
